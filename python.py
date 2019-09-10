502a84638292419985c76437b66bf107bef5275b
MOODY Greg <gmoody@altranuk.local>
2019-09-10 09:49:25 +0100

    Manually searches each word and returns all softwire email addresses

--- python.py
+++ python.py
@@ -1,1 +1,14 @@
-print("Helloooo")
+import re
+
+file = open("sample.txt", 'r')
+text=file.read()
+output=[]
+
+text=text.split()
+for word in text:
+    if "@softwire.com" in word:
+        output.append(word)
+
+print(text)
+print(output)
+
